2484234afe9f8fef8e0859d72b276664078d7fbb
Ahmed  Yahia <ahmed.yahia.hakeem@gmail.com>
2024-02-12 15:47:42 +0200

    Fix precomilation issue

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Friends
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    # config/application.rb
+    config.assets.enabled = true
+    config.assets.compile = true
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
